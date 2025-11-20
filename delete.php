a14745c968917c36f0a744c19c36afc18c22af08
Dhamar <dhmardwi@gmail.com>
2025-11-20 12:08:57 +0700

    nambahin fitur hapus

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php
+include 'connectToDb.php';
+
+$id = $_GET['id'];
+
+$hasilHapus = mysqli_query($db, "DELETE FROM siswa WHERE id='$id'");
+
+if ($hasilHapus) {
+    echo "
+    <script>
+        alert('Data berhasil dihapus.');
+        window.location='index.php';
+    </script>;
+    ";
+}else {
+    echo "
+    <script>
+        alert('Data berhasil dihapus.');
+        window.location='index.php';
+    </script>;
+    "; 
+}
